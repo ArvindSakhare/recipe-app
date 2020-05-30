da46153949f054e21a1294f37a71d58d256477d2
ArvindSakhare <arvindsakhare1@gmail.com>
2020-05-30 17:45:01 +0530

    content changes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,15 +9,16 @@ export class RecipeService{
   
     recipes: Recipe[] = [
         new Recipe('A Pizza', 
-        'This is test recipe', 
+        'This is Pizza recipe', 
         'https://www.biggerbolderbaking.com/wp-content/uploads/2019/07/15-Minute-Pizza-WS-Thumbnail.png',
          [
-             new Ingredient('Beans', 10)
+             new Ingredient('Beans', 10),
+             new Ingredient('Cheese', 2)
          ]   
         ),
-        new Recipe('Another Test Recipe', 
-        'This is Another recipe', 
-        'https://www.biggerbolderbaking.com/wp-content/uploads/2019/07/15-Minute-Pizza-WS-Thumbnail.png',
+        new Recipe('A Big Fat Burger', 
+        'This is A Big Fat Burger  recipe', 
+        'https://media1.s-nbcnews.com/j/newscms/2019_21/2870431/190524-classic-american-cheeseburger-ew-207p_d9270c5c545b30ea094084c7f2342eb4.fit-760w.jpg',
         [
             new Ingredient('Buns', 5),
             new Ingredient('French fries', 20)
