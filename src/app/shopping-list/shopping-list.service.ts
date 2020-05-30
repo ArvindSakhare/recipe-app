36ec3e3cca155b9679ef547d13721f6c92f892ba
ArvindSakhare <arvindsakhare1@gmail.com>
2020-05-30 19:17:35 +0530

    Added editIngredient functionality

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -4,22 +4,30 @@ import  { Subject } from 'rxjs';
 
 export class ShoppingListService{
     ingredientChanged = new Subject<Ingredient[]>();
-
+    startedEditing = new Subject<number>();
     ingredients: Ingredient[] = [
         new Ingredient('Apples', 5),
         new Ingredient('Tomatoes', 10)
       ];
 
+      getIngredient(index: number){
+        return this.ingredients[index];
+      }      
       getIngredients(){
         return  this.ingredients.slice();
       }
+
       addIngredient(ingredient: Ingredient){
           this.ingredients.push(ingredient);
           this.ingredientChanged.next(this.ingredients.slice());
       }
-
       addIngredients(ingredients: Ingredient[]){
         this.ingredients.push(... ingredients);
+        this.ingredientChanged.next(this.ingredients.slice());
+      }
+
+      updateIngredient(index: number, newIngredient: Ingredient){
+        this.ingredients[index] = newIngredient;
         this.ingredientChanged.next(this.ingredients.slice());
       }
 }
