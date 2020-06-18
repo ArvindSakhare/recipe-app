43639529d380df1d4b45f737a5432515f3b02654
ArvindSakhare <arvindsakhare1@gmail.com>
2020-06-18 20:15:41 +0530

    Added a recipes-resolver

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -13,6 +13,6 @@ export class HeaderComponent{
         this.dataStoreService.storeRecipes();
     }
     onFetchData(){
-        this.dataStoreService.fetchRecipes();
+        this.dataStoreService.fetchRecipes().subscribe();
     }
 }
--- src/app/shared/data-store.service.ts
+++ src/app/shared/data-store.service.ts
@@ -1,32 +1,34 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators'
+import { map, tap } from 'rxjs/operators'
 
 import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class DataStoreService {
-    constructor(private http:HttpClient, private recipeService: RecipeService) {}
+    constructor(private http: HttpClient, private recipeService: RecipeService) { }
 
-    storeRecipes(){
+    storeRecipes() {
         const recipes = this.recipeService.getRecipes();
-        this.http.put("https://angular-recipes-5c5df.firebaseio.com/recipe.json",recipes)
-        .subscribe(response => {
-            console.log(response);
-        });
+        this.http.put("https://angular-recipes-5c5df.firebaseio.com/recipe.json", recipes)
+            .subscribe(response => {
+                console.log(response);
+            });
     }
 
-    fetchRecipes(){
-        const recipes = this.http.get<Recipe[]>("https://angular-recipes-5c5df.firebaseio.com/recipe.json")
-        .pipe(map(recipes => {
-            return recipes.map(recipes => {
-                return { ...recipes, ingredients: recipes.ingredients ? recipes.ingredients : [] };
-            })
-        }))
-        .subscribe(recipes => {
-            this.recipeService.setRecipes(recipes);          
-        });              
+    fetchRecipes() {
+        return this.http.get<Recipe[]>("https://angular-recipes-5c5df.firebaseio.com/recipe.json")
+            .pipe(
+                map(recipes => {
+                    return recipes.map(recipes => {
+                        return { ...recipes, ingredients: recipes.ingredients ? recipes.ingredients : [] };
+                    })
+                }),
+                tap(recipes => {
+                    this.recipeService.setRecipes(recipes);
+                })
+            )
     }
 
 }
